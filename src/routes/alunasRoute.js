4e3684dc769a2c0a1e972a21b73e9dadeae6f111
brunarbcampos <brunarbcampos@gmail.com>
2019-10-08 20:03:41 -0300

    Subindo a API

---
+++ src/routes/alunasRoute.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+/*const alunas = require('../model/alunas.json')*/
+const controller = require ('../controllers/alunasController')
+
+router.get('/', controller.get)
+/*router.get('/', function(request, response){
+    response.status(200).send(alunas)
+}) */ //tiramos pq o controller puxa isso agora
+router.get ('/:id', controller.getById)
+
+module.exports =  router
